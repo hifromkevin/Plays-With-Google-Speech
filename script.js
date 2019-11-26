bf2fa0050bc9fa1a4aff115aeea7010303280747
hifromkevin <lam.kevin86@gmail.com>
2019-11-25 20:08:55 -0800

    Plays with promise

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const speech = require('@google-cloud/speech');
 const fs = require('fs');
+// import { speech } from '@google-cloud/speech';
+// import fs from 'fs';
 
 async function main() {
   const client = new speech.SpeechClient();
@@ -26,6 +28,11 @@ async function main() {
   const [response] = await client.recognize(request);
   const transcription = response.results.map(results => results.alternatives[0].transcript).join('\n');
   console.log('T', transcription);
+  return transcription;
 }
 
-main()
+let f = main().then(x => console.log('!',x))
+
+console.log('f gon give it to ya', f);
+
+// document.getElementById('lit').innerHTML = f;
